4c10571e261443b8c6b201497da59c0ce89f987b
JoacoMaurtua <joacomaurtua@gmail.com>
2021-10-13 22:06:01 -0500

    coins list structure

--- finance-price-traker/src/components/Coins/Coins.js
+++ finance-price-traker/src/components/Coins/Coins.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Coins.css';
 
-const Coins = ({name}) => {
+const Coins = ({name, price, symbol, marketcap, volumen, image, priceChange, id}) => {
   return (
-    <div>
-      <h1>{name}</h1>
+    <div className="coin_container">
+      <div className="coin_row">
+        <div className="coin">
+          <img src={image}  alt="coin_img"/>
+          <h1 className="coin_h1">{name}</h1>
+          <p className="coin_symbol">{symbol}</p>
+        </div>
+        <div className="coin_data">
+          <p className="coin_price">${price}</p>
+        </div>
+      </div>
     </div>
   );
 }
