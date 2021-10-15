2129cb805d35cd82a36bac545f957d098c318019
JoacoMaurtua <joacomaurtua@gmail.com>
2021-10-15 09:51:04 -0500

    layout.js

--- finance-price-traker/src/actions/coinsDB.js
+++ finance-price-traker/src/actions/coinsDB.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
-const getCryptoData = async() =>{
-  try{
-    const getData = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+const getCryptoData = async () => {
+  try {
+    const getData = await axios.get(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+    );
     return getData;
-
-  }catch(err){
-    console.log('Error',err)
+  } catch (err) {
+    console.log('Error', err);
   }
-}
+};
 
 export default getCryptoData;
--- finance-price-traker/src/pages/MainPage/Main.js
+++ finance-price-traker/src/pages/MainPage/Main.js
@@ -1,12 +1,10 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import CoinList from '../../components/CoinList/CoinList';
 import Searchbar from '../../components/SearchBar/SearchBar';
 import getCryptoData from '../../actions/coinsDB';
 
-
-
 const Main = () => {
-  const [cryptoData,setcryptoData] = useState([]);
+  const [cryptoData, setcryptoData] = useState([]);
 
   /* useEffect(() =>{
     axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
@@ -14,24 +12,21 @@ const Main = () => {
          .catch(err => console.error(err))
   },[]) */
 
-  useEffect(() =>{
-    const loadingData =async()=>{
+  useEffect(() => {
+    const loadingData = async () => {
       const coins = await getCryptoData();
       setcryptoData(coins.data);
-    }
+    };
     loadingData();
-  },[]); 
+  }, []);
 
-  
   console.log(cryptoData);
   return (
     <div>
-      <Searchbar/>
-      <CoinList filteredCoins={cryptoData}/>
+      <Searchbar />
+      <CoinList filteredCoins={cryptoData} />
     </div>
   );
-}
+};
 
 export default Main;
-
-
