c13b126a3e32734df10db153306f5debd571f398
JoacoMaurtua <joacomaurtua@gmail.com>
2022-01-26 14:02:37 -0500

    terminada version beta

--- finance-price-traker/src/pages/CoinPage/CoinPage.js
+++ finance-price-traker/src/pages/CoinPage/CoinPage.js
@@ -28,7 +28,7 @@ const Coinpage = () => {
         <img src={logo} alt="logo" onClick={onClick} style={{ width: '20%' }} />
       </div>
       <div className="coin_container">
-        {/* <img src={coin.image.large} alt={coin.name} className="coin_image" /> */}
+       {/*  <img src={coin.image.large} alt={coin.name} className="coin_image" />  */}
         <h1 className="coin_name">{coin.name}</h1>
         <p className="coin_ticker">{coin.symbol}</p>
      {/*  <p className="coin_current">{coin.market_data.current_price.usd}</p>  */}
